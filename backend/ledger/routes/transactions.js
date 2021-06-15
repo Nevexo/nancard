907f8d1fcde313eaa5cf2992f3da0f33040d9223
Cameron Fleming <cameron@nevexo.space>
2021-06-15 15:58:33 +0100

    Add authorization to transactions

--- backend/ledger/routes/transactions.js
+++ backend/ledger/routes/transactions.js
@@ -9,8 +9,10 @@ export const router = Router();
 export const events = new EventEmitter();
 
 router.get("/api/users/:id/transactions", async (req, res) => {
+  if (!req.headers.authorization) return res.status(401).json({"error": "No authorization header"});
   const user = await User.findOne({"id": req.params.id});
   if (!user) return res.status(404).json({"error": "Invalid user specified"});
+  if (user.web.access_token != req.headers.authorization) return res.status(401).json({"error": "Invalid authorisation token"});
 
   const TargetTransactions = await Transaction.find({"target": user.id});
   const InitTransactions = await Transaction.find({"initator": user.id});
